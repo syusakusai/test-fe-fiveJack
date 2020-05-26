af32e3a8d50eacc96d81cc25344632047b5fdd56
syusakusai <bapamu55@gmail.com>
2020-05-26 12:45:34 +0700

    Update test-fe.js

--- test-fe.js
+++ test-fe.js
@@ -86,33 +86,33 @@ console.log(solution(4, [4, 4, 4,4, 4, 4]));
 
 
 function solution(relation) {
-    const column = relation[0].length
-    const rows = relation.length
-    const sets = 1 << column
-    const answer = new Set()
+    const column = relation[0].length;
+    const rows = relation.length;
+    const sets = 1 << column;
+    const answer = new Set();
 
     for (let i=1; i<sets; i++) {
-        const tmp = new Set()
+        const tmp = new Set();
         for (let row=0; row<rows; row++) {
-            let key = ''
+            let key = '';
             for (let col=0; col<column; col++) {
-                if (i & (1 << col)) key = String(key) + String(relation[row][col])
+                if (i & (1 << col)) key = String(key) + String(relation[row][col]);
             }
-            tmp.add(key)
+            tmp.add(key);
         }
-        if (tmp.size === rows) answer.add(i)
+        if (tmp.size === rows) answer.add(i);
     }
 
     for (let i of answer) {
         for (let j of answer) {
-            if (i >= j) continue
-            if ((i & j) === i) answer.delete(j)
+            if (i >= j) continue;
+            if ((i & j) === i) answer.delete(j);
         }
     }
 
     // console.log(Array.from(answer).map(e => e.toString(2)))
 
-    return answer.size
+    return answer.size;
 }
 
 solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]])
